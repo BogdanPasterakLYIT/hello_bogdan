43794853443e78d3bbd8c696f98fd97b3b324bcb
bogdanpasterak <bogdanpasterak@gmail.com>
2023-03-21 19:14:22 +0000

    colors

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,11 +10,18 @@ export class LoginComponent {
   name = '';
   image = 'assets/img/icon.png';
   euro = 2.2;
+  bg = 'background-color: rgb(0, 0, 0)';
+  colorR = 10;
 
   onClick() {
     console.log(this.name);
   }
   onKeyUp(e: any) {
     console.log(e.target.value);
+  }
+
+  onChange() {
+    console.log(this.colorR);
+    this.bg = `background-color: rgb(${this.colorR}, 0, 0)`;
   }
 }
