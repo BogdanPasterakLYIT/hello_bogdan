a1d1285d75e8949cbece49087b007d5e3c6c50ae
BogdanPasterak <L00157106@student.lyit.ie>
2023-03-29 10:43:25 +0100

    counter

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,6 +12,7 @@ export class LoginComponent {
   euro = 2.2;
   bg = 'background-color: rgb(0, 0, 0)';
   colorR = 10;
+  v = 3;
 
   onClick() {
     console.log(this.name);
@@ -23,5 +24,16 @@ export class LoginComponent {
   onChange() {
     console.log(this.colorR);
     this.bg = `background-color: rgb(${this.colorR}, 0, 0)`;
+  }
+
+  run() {
+    setTimeout(() => this.minus(), 1000);
+  }
+
+  minus() {
+    if (this.v > 0) {
+      this.v = this.v - 1;
+      this.run();
+    }
   }
 }
